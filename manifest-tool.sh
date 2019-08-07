0085cdffa1bf2c86284f32f6a7c6b8159449f124
snowdream <yanghui1986527@gmail.com>
2019-08-07 20:40:24 +0800

    Update: frp latest

--- manifest-tool.sh
+++ manifest-tool.sh
@@ -3,6 +3,8 @@
 
 # frpc
 manifest-tool push from-spec frpc.yaml
+manifest-tool push from-spec frpc-latest.yaml
 
 # frps
 manifest-tool push from-spec frps.yaml
+manifest-tool push from-spec frps-latest.yaml
