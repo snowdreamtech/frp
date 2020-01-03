e74f1904864650c8260e5a27e4fe3fe12503d5ac
snowdream <sn0wdr1am@icloud.com>
2020-01-03 22:10:10 +0800

    Add: 0.31.0

--- manifest-tool.sh
+++ manifest-tool.sh
@@ -3,8 +3,6 @@
 
 # frpc
 manifest-tool push from-spec frpc.yaml
-manifest-tool push from-spec frpc-latest.yaml
 
 # frps
 manifest-tool push from-spec frps.yaml
-manifest-tool push from-spec frps-latest.yaml
