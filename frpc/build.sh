1e325f4a7720a1fc2d7791856a553227a1642eee
snowdream <sn0wdr1am@qq.com>
2024-07-03 17:25:09 +0800

    feat: 0.58.0 with docker buildx

--- frpc/build.sh
+++ frpc/build.sh
@@ -4,16 +4,14 @@ DOCKER_HUB_PROJECT=snowdreamtech/frpc
 
 GITHUB_PROJECT=ghcr.io/snowdreamtech/frpc
 
-docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x-dev\
--t ${DOCKER_HUB_PROJECT}:latest-dev\
--t ${DOCKER_HUB_PROJECT}:0.58.1-r1-dev\
--t ${DOCKER_HUB_PROJECT}:0.58.1-dev\
--t ${DOCKER_HUB_PROJECT}:0.58-dev\
--t ${DOCKER_HUB_PROJECT}:0-dev\
--t ${GITHUB_PROJECT}:latest-dev\
--t ${GITHUB_PROJECT}:0.58.1-r1-dev\
--t ${GITHUB_PROJECT}:0.58.1-dev\
--t ${GITHUB_PROJECT}:0.58-dev\
--t ${GITHUB_PROJECT}:0-dev\
+docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x\
+-t ${DOCKER_HUB_PROJECT}:latest\
+-t ${DOCKER_HUB_PROJECT}:0.58.1\
+-t ${DOCKER_HUB_PROJECT}:0.58\
+-t ${DOCKER_HUB_PROJECT}:0\
+-t ${GITHUB_PROJECT}:latest\
+-t ${GITHUB_PROJECT}:0.58.1\
+-t ${GITHUB_PROJECT}:0.58\
+-t ${GITHUB_PROJECT}:0\
 .\
 --push
